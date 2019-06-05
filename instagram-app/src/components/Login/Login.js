7b43e555eb1aa64f86d81fb78bfccd2867aad27f
oyekunle-mark <oyekunlemac@gmail.com>
2019-06-05 13:16:00 +0100

    Created the component to render the login form and register users

---
+++ instagram-app/src/components/Login/Login.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function Login() {
+  const login = e => {
+    e.preventDefault();
+    const username = document.querySelector(".usernameInput").value;
+
+    localStorage.instagramCloneUsername = username;
+    window.location.reload();
+  };
+
+  return (
+    <form onSubmit={login}>
+      <input className="usernameInput" type="text" placeholder="username" />
+      <input type="text" placeholder="password" />
+      <button type="submit">Login</button>
+    </form>
+  );
+}
